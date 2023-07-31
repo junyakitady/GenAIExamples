196c98b3386172ebf8340a32642b9d3ec87bd1a1
Junya Kitada <junyakitady@google.com>
2023-07-31 18:43:04 +0900

    don't share data between 2 pages

--- pages/ChatEnterpriseSearch.py
+++ pages/ChatEnterpriseSearch.py
@@ -15,32 +15,32 @@ SEARCH_ENGINE_ID = "<search_engine_id>"
 vertexai.init(project=PROJECT_ID, location=REGION)
 
 st.title("Chat Enterprise Search")
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+if "esmessages" not in st.session_state:
+    st.session_state.esmessages = []
 
-# Text model instance integrated with LangChain
-llm = VertexAI(model_name="text-bison@001", max_output_tokens=256, temperature=0.2, top_k=40, top_p=0.8, verbose=True)
-# Enterprise Search retriever
-retriever = GoogleCloudEnterpriseSearchRetriever(project_id=PROJECT_ID, search_engine_id=SEARCH_ENGINE_ID, get_extractive_answers=False)
-# Create chain to answer questions
-#st.session_state.rqa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-st.session_state.rqa = ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever, memory=memory,)
+    # Text model instance integrated with LangChain
+    llm = VertexAI(model_name="text-bison@001", max_output_tokens=256, temperature=0.2, top_k=40, top_p=0.8, verbose=True)
+    # Enterprise Search retriever
+    retriever = GoogleCloudEnterpriseSearchRetriever(project_id=PROJECT_ID, search_engine_id=SEARCH_ENGINE_ID, get_extractive_answers=False)
+    # Create chain to answer questions
+    #st.session_state.rqa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    st.session_state.esqa = ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever, memory=memory,)
 
 # Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+for esmessages in st.session_state.esmessages:
+    with st.chat_message(esmessages["role"]):
+        st.markdown(esmessages["content"])
 
 # Accept user input
-if prompt := st.chat_input("Ask something here", disabled=("rqa" not in st.session_state)):
+if prompt := st.chat_input("Ask something here"):
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
 
     # RetrievalQA
-    qa = st.session_state.rqa
-    response = qa({"question": prompt}) #use "query" for RetrievalQA
+    esqa = st.session_state.esqa
+    response = esqa({"question": prompt}) #use "query" for RetrievalQA
 
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
@@ -53,5 +53,5 @@ if prompt := st.chat_input("Ask something here", disabled=("rqa" not in st.sessi
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
     # Add message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.session_state.messages.append({"role": "assistant", "content": response["answer"]})
+    st.session_state.esmessages.append({"role": "user", "content": prompt})
+    st.session_state.esmessages.append({"role": "assistant", "content": response["answer"]})
